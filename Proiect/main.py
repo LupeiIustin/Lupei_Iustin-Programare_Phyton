152a19b13e93bf570e46886dc29377f92558499a
Lupei Iustin <lupeiiustin@gmail.com>
2022-12-15 17:03:06 +0200

    Update AI
    MinMax algorithm for impossible AI
    Medium AI difficulty (random+minmax)

--- Proiect/main.py
+++ Proiect/main.py
@@ -115,12 +115,76 @@ class AI:
 
         return empty_sqrs[idx]  # (row, col)
 
-    def eval(self, main_board, random):
-        if self.level == 0:
-            # random choice
-            eval = 'random'
-            move = self.rnd(main_board)
-        return move  # row, col
+        # --- MINIMAX ---
+
+        def minimax(self, board, maximizing):
+
+            # terminal case
+            case = board.final_state()
+
+            # player 1 wins
+            if case == 1:
+                return 1, None  # eval, move
+
+            # player 2 wins
+            if case == 2:
+                return -1, None
+
+            # draw
+            elif board.isfull():
+                return 0, None
+
+            if maximizing:
+                max_eval = -100
+                best_move = None
+                empty_sqrs = board.get_empty_sqrs()
+
+                for (row, col) in empty_sqrs:
+                    temp_board = copy.deepcopy(board)
+                    temp_board.mark_sqr(row, col, 1)
+                    eval = self.minimax(temp_board, False)[0]
+                    if eval > max_eval:
+                        max_eval = eval
+                        best_move = (row, col)
+
+                return max_eval, best_move
+
+            elif not maximizing:
+                min_eval = 100
+                best_move = None
+                empty_sqrs = board.get_empty_sqrs()
+
+                for (row, col) in empty_sqrs:
+                    temp_board = copy.deepcopy(board)
+                    temp_board.mark_sqr(row, col, self.player)
+                    eval = self.minimax(temp_board, True)[0]
+                    if eval < min_eval:
+                        min_eval = eval
+                        best_move = (row, col)
+
+                return min_eval, best_move
+
+        # --- MAIN EVAL ---
+
+        def eval(self, main_board, random):
+            if self.level == 0:
+                # random choice
+                eval = 'random'
+                move = self.rnd(main_board)
+            elif self.level == 1:
+                # minimax algo choice
+                eval, move = self.minimax(main_board, False)
+            elif self.level == 2:
+                # min max and random algo ~ medium dificulty
+                if random % 2 == 0:
+                    eval = 'random'
+                    move = self.rnd(main_board)
+                else:
+                    eval, move = self.minimax(main_board, False)
+                print(self.player)
+            print(move, eval)
+
+            return move  # row, col
 
 class Game:
 
